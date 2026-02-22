0907ddd1e51fd60919c075b50b92923ea4316d83
MdOnim <mdonim795@gmail.com>
2026-02-23 01:36:28 +0600

    80% complete

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let Interviewbox =[];
 let Rejectedbox =[];
+let currentStatus ='all';
 
 // part 1 total 
 // Total ,Interview , Rejected niye aslam
@@ -43,6 +44,8 @@ function toggleStyle(id){
     
 
     const selected = document.getElementById(id)
+    currentStatus=id
+
     selected.classList.remove('bg-[#FFFFFF]', 'text-black')
     selected.classList.add('bg-[#3B82F6]' ,'text-white')
 
@@ -50,10 +53,16 @@ function toggleStyle(id){
     if(id=='Interview-filter-btn'){
         allcardParent.classList.add('hidden');
         filterSectoin.classList.remove('hidden')
+        renderInterview()
 
     }else if(id=='all-filter-btn'){
         allcardParent.classList.remove('hidden');
         filterSectoin.classList.add('hidden')
+    }else if(id== 'Rejected-filter-btn'){
+        allcardParent.classList.add('hidden');
+        filterSectoin.classList.remove('hidden')
+        renderrejected()
+        
     }
 
 
@@ -67,7 +76,7 @@ function toggleStyle(id){
 
 mainContainer.addEventListener('click',function(event){
 
-    console.log(event.target.classList.contains('interview-btn'))
+    // console.log(event.target.classList.contains('interview-btn'))
     
     if(event.target.classList.contains('interview-btn')){
     
@@ -92,16 +101,54 @@ mainContainer.addEventListener('click',function(event){
     // find deye chek kore deklam arrr ar vitore info ache kina
     
     const mobileShopExist = Interviewbox.find(item=> item.mobileShop == cardInfo.mobileShop )
-    
-    
     if(!mobileShopExist){
         Interviewbox.push(cardInfo)
     }
-    renderInterview ()
+
+    Rejectedbox = Rejectedbox.filter(item=> item.mobileShop != cardInfo.mobileShop)
+    calculateCount()
+    
+    if( currentStatus=='Rejected-filter-btn'){
+       renderrejected()
+     }
+    
+
+
+    }else if(event.target.classList.contains('rejected-btn')){
+    
+    const parentNode = event.target.parentNode.parentNode;
+    const mobileShop = parentNode.querySelector('.mobile-shop').innerText
+    const native =parentNode.querySelector('.native-Developer').innerText
+    const fullTime =parentNode.querySelector('.full-time').innerText
+    const notApplicate =parentNode.querySelector('.Not-Applied-button').innerText
+    const cross =parentNode.querySelector('.cross-platform').innerText
+
+    // majkkhaaer je ont applicate oita chanage akhane hoi
+    parentNode.querySelector('.Not-Applied-button').innerText ='Rejectedbox'
+    
+    const cardInfo ={
+        mobileShop,
+        native,
+        fullTime,
+        notApplicate:'Rejected',
+        cross
     }
-        
+    
+    // find deye chek kore deklam arrr ar vitore info ache kina
+    
+    const mobileShopExist = Rejectedbox.find(item=> item.mobileShop == cardInfo.mobileShop )
+    if(!mobileShopExist){
+        Rejectedbox.push(cardInfo)
+    }
+     Interviewbox = Interviewbox.filter(item=> item.mobileShop != cardInfo.mobileShop)
 
-
+     if( currentStatus=='Interview-filter-btn'){
+        renderInterview ()
+     }
+    calculateCount()
+    
+    
+    }
 })
 
 
@@ -122,6 +169,42 @@ function renderInterview (){
                     <p class="native-Developer text-[#64748B]">React Native Developer</p>
                     <p class="full-time py-4 text-[#64748B]">Remote . Full-time . $130,000 - $175,000</p>
                     <button class="Not-Applied-button btn btn-active">${interview.notApplicate}</button>
+                    <p class="cross-platform py-3">Build cross-platform mobile applications using React Native. Work on products used
+                        by millions of users worldwide.</p>
+                    <div class="flex gap-5">
+                        <button class="interview-btn btn btn-outline btn-success">interview</button>
+                        <button class="rejected-btn btn btn-outline btn-error">Rejected</button>
+                    </div>
+                </div>
+    
+        `
+        filterSectoin.appendChild(div)
+    }
+
+}
+
+
+
+
+
+
+
+
+// rejected 
+function renderrejected (){
+    // aysecton tumo faka thakako nojobs id deye dekho kaj kore kina
+    filterSectoin.innerText=''
+    for (let rejected of Rejectedbox){
+        console.log(interview)
+        let div = document.createElement('div');
+        div.className ='job-card flex justify-between border border-gray-300 rounded-sm shadow-lg bg-[#FFFFFF] md:gap-9'
+        div.innerHTML=`
+        
+        <div class="p-5">
+                    <h2 class="mobile-shop text-[#002C5C] text-[20px] font-semibold">${rejected.mobileShop}</h2>
+                    <p class="native-Developer text-[#64748B]">React Native Developer</p>
+                    <p class="full-time py-4 text-[#64748B]">Remote . Full-time . $130,000 - $175,000</p>
+                    <button class="Not-Applied-button btn btn-active">${rejected.notApplicate}</button>
                     <p class="cross-platform py-3">Build cross-platform mobile applications using React Native. Work on products used
                         by millions of users worldwide.</p>
                     <div class="flex gap-5">
